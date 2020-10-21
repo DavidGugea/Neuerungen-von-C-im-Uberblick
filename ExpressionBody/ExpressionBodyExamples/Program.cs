2dca58ef1889fe330a0b29a3a37e7a046d920757
David Gugea <davidgugea@yahoo.com>
2020-10-21 22:36:52 +0200

    Expression Body examples + understood how to use it

---
+++ ExpressionBody/ExpressionBodyExamples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExpressionBodyExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person David = new Person("David", 17);
+
+            Console.WriteLine(David.GetInfo());
+            David.SayHello();
+        }
+    }
+    public class Person
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public string GetInfo() => $"{this.Name} is {this.Age} years old";
+        public void SayHello() => Console.WriteLine($"{this.Name} : hello");
+
+        public Person() { }
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
