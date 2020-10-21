c9b729fca47db1e6bdf69ebcb32db95c7e7b82c3
David Gugea <davidgugea@yahoo.com>
2020-10-21 22:43:29 +0200

    Using Static example ( like ' from module import * ' in python 3 )

---
+++ UsingStatic/UsingStaticExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Math; // You simply don't have to write Math.Sqrt for example anymore
+// using static System.Math is the same like from math import * in python 3 for example
+
+namespace UsingStaticExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sqrt(9));
+            Console.WriteLine(Abs(-2));
+            Console.WriteLine(Abs(2));
+        }
+    }
+}
