c191c48dab5e30b35084a7072e4cfbd30263efd6
David Gugea <davidgugea@yahoo.com>
2020-10-22 20:41:58 +0200

    Null Operator & how to avoid the NullReferenceException & how to use the ?? to make sure that a variable will still have a value that is not null

---
+++ NullOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NullOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Person person = new Person() { Name = "X" };
+            Person person = null;
+            // not a problem even if person is null. But if we say string name = person.Name and person is null then we will get NullReferenceException error.
+            // If we use ?. we won't get any error, and if person is null and we still want to string name to have a value and not null then we can use the ?? operator
+            string name = person?.Name ?? "Ioannis";
+
+            Console.WriteLine(name);
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
