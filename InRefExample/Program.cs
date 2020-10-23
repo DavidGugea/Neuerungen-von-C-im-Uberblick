661c426ec647ce811fe04b9f60b3328bf890797a
David Gugea <davidgugea@yahoo.com>
2020-10-23 21:07:01 +0200

    in & ref operators example

---
+++ InRefExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InRefExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            function_raw(x);
+            Console.WriteLine(x);
+        }
+        static void function_ref(ref int argument)
+        {
+            argument++;
+            Console.WriteLine(argument);
+        }
+        static void function_in(in int argument)
+        {
+            // "argument" is readonly now so therefore, argument++ is wrong
+            Console.WriteLine(argument);
+        }
+        static void function_raw(int argument)
+        {
+            argument++;
+            Console.WriteLine(argument);
+        }
+    }
+}
